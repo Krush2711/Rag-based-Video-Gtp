1261f4f411ceaf8e452ddc36f0b738b768b2d75f
Krushna <krushnapalekar27@gmail.com>
2025-12-04 22:46:24 +0530

    json files complited

--- 2_audio_to_segments.py
+++ 2_audio_to_segments.py
@@ -12,13 +12,14 @@ audios = os.listdir("audios")
 
 for audio in audios:
     num = audio.split(" ")[0]
-    res = model.transcribe(audio= "outputfile.mp3", language='en')
+    res = model.transcribe(audio= f"audios/{audio}", language='en')
+    print(f"working with the {audio}")
     chunks = []
     for segment in res['segments']:
-        chunks.append({"number":1, "start:":segment["start"],"end":segment["end"], "text":segment["text"]})
+        chunks.append({"number":num, "start:":segment["start"],"end":segment["end"], "text":segment["text"]})
 
     chunks_with_meta_data = {"chunks":chunks, "text" : res["text"]}
-    print(chunks)
 
-    with open("output.json", "w") as f:
+    with open(f"jsons/{num}.json", "w") as f:
         json.dump(chunks_with_meta_data, f)
+    print(f"done with the {num}")
